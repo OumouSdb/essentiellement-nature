8a801ae354eaaa41d16c39f58a35e12fde6003c0
Oumou <oumou-niakate@live.fr>
2022-08-06 17:10:27 +0200

    connexion, dashbord

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from '../app/home/home.component';
+import { ContactComponent } from '../app/contact/contact.component';
+import { LoginComponent } from '../app/login/login.component';
+import { SubscribeComponent } from '../app/subscribe/subscribe.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'connexion', component: LoginComponent },
+  { path: 'inscription', component: SubscribeComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'customer-dashboard', component: CustomerDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from '../app/component/navbar/navbar.component';
+import { HomeComponent } from '../app/home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    FooterComponent,
+    LoginComponent,
+    SubscribeComponent,
+    AdminDashboardComponent,
+    CustomerDashboardComponent,
   ],
   imports: [
     BrowserModule,
