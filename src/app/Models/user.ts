7cd7ed57dfaed2b9ce3caf9acf3eb170677ce12f
Oumou Niakate <oumou-niakate@live.fr>
2023-12-23 00:47:18 +0100

    services, model user

---
+++ src/app/Models/user.ts
@@ -0,0 +1,19 @@
+export class User {
+
+    id: number;
+    firstname: string;
+    lastName: string;
+    age: number;
+    sexe: string;
+
+
+    constructor() {
+        this.id = 0;
+        this.firstname = "",
+            this.lastName = "";
+        this.age = 0;
+        this.sexe = "";
+
+    }
+
+}
